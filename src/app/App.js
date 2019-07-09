9ebc4b79f6fc477bf836ad4f3f07f52533bc0e98
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-09 23:11:00 +0530

    added project structure

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Welcome from "./Welcome";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/Welcome.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Welcome() {
+  return <h1>Welcome to Crypto-Dashboard</h1>;
+}
