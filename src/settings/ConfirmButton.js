8ac64ca522d7c8ff66da1c7242a7785c21d74884
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-10 08:34:29 +0530

    created and styled coin grid

--- src/settings/ConfirmButton.js
+++ src/settings/ConfirmButton.js
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../app/AppProvider";
+import { fontSize1, greenBoxShadow, color3 } from "../shared/Styles";
 
 const ConfirmButtonStyle = styled.div`
   margin: 20px;
-  color: green;
+  color: ${color3};
+  ${fontSize1};
   cursor: pointer;
+  &:hover {
+    ${greenBoxShadow}
+  }
 `;
 
 const CenterDiv = styled.div`
