8a466b77bc89cc4c49e71bc8bb115a67985b3cf6
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-10 07:24:19 +0530

    Added Page for routing

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Welcome from "./Welcome";
 import ConfirmButton from "./ConfirmButton";
+import Page from "../shared/Page";
 export default function() {
   return (
-    <div>
+    <Page name="Settings">
       <Welcome />
       <ConfirmButton />
-    </div>
+    </Page>
   );
 }
